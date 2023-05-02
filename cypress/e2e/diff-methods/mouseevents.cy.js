2460fc7b4564f50ff0913861017cc7d9a8b034f9
UsmanKarim08 <muhammadkarim1312@gmail.com>
2023-05-02 19:58:13 -0400

    mouse events

--- cypress/e2e/diff-methods/mouseevents.cy.js
+++ cypress/e2e/diff-methods/mouseevents.cy.js
@@ -1,5 +1,11 @@
 describe('mouse event suite', () => {
-    it('Mouse event -Right click, Mouse over, doubleclick', () => {
-        
+    it('should perform mouse events', () => {
+        cy.visit('https://www.programsbuzz.com/')
+        cy.contains('Start Learning').rightclick();
+       cy.wait(2000)
+        cy.log('Check the context menu')
+        cy.contains('Tutorials').trigger('mouseover');
+        cy.wait(2000)
+        cy.contains('Contact').dblclick()
     })
 })
