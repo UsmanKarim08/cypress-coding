3775f60fd4c41b41981fe90e7f87b0cc7f832076
UsmanKarim08 <muhammadkarim1312@gmail.com>
2023-05-05 19:23:27 -0400

    added mocha awesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     env:{
       "username": "Osman Karim",
@@ -13,6 +14,7 @@ module.exports = defineConfig({
     // "baseUrl" : 'https://demoqa.com/text-box',
     "baseUrl" : 'https://opensource-demo.orangehrmlive.com/',
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       return require("./cypress/plugins/index.js")(on, config)
       // implement node event listeners here
     },
